b157add2a32ae440fb7c101854ab50294f6bef62
Phil <philrwebb@gmail.com>
2020-11-23 13:00:56 +0800

    completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,30 @@
 <script>
-import Timer from "./Timer.svelte";
-import HowTo from "./HowTo.svelte";
-const timerEnds = (e) => {
-	console.log(e);
-	audio.play();
-}
-let audio;
+   import Timer from './Timer.svelte';
+   import HowTo from './HowTo.svelte';
+   const timerEnds = (e) => {
+      audio.play();
+   };
+   let audio;
 </script>
+
 <style>
-h1,
-h3 {
-	text-align: center;
-}
+   h1,
+   h3 {
+      text-align: center;
+   }
 </style>
+
 <h1>Handwashing App</h1>
-<div>
-</div>
-<Timer on:end={timerEnds}/>
+<div />
+<Timer on:end={timerEnds} />
 <HowTo />
 
 <h3>
-<a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
-<a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
+   <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
+   <a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound
+      Source</a>
 </h3>
 
-<audio bind:this={audio} src="sound.wav"></audio>
+<audio bind:this={audio} src="sound.wav">
+   <track kind="captions" />
+</audio>
